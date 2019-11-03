57ddfa1f3d1e83f02ae6a6f1af567872738854af
Qitnyatt <Qitnyatt@protonmail.com>
2019-11-03 00:33:33 +0000

    Added bot itself

---
+++ qitnyatt_telegram_echo_bot.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import logging
+
+from telegram.ext import CommandHandler
+from telegram.ext import Filters
+from telegram.ext import MessageHandler
+from telegram.ext import Updater
+
+FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(
+    format=FORMAT,
+    level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+def start(update, context):
+    first_name = update.message.from_user.first_name
+    last_name = update.message.from_user.last_name
+    update.message.reply_text(f'Hello, {first_name} {last_name}.')
+
+
+def help_(update, context):
+    update.message.reply_text('sorry i can\'t help you.')
+
+
+def echo(update, context):
+    update.message.reply_text(f'>>> {update.message.text}')
+
+
+def error(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    token = input('TOKEN:')
+    updater = Updater(token, use_context=True)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('help', help_))
+    dispatcher.add_handler(MessageHandler(Filters.text, echo))
+    dispatcher.add_error_handler(error)
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
